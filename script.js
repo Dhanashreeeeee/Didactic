596bc45b3819762a3fced810833cffa9662e2434
Dhanashreeeeee <111290440+Dhanashreeeeee@users.noreply.github.com>
2022-09-20 21:35:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Adding text to paragraph 3
+document.getElementById("p3").innerHTML = "Welcome to JavaScript 1";
+// Adding color to JavaScript paragraph
+tdocument.getElementById('JavaScript').style.color='red';
+// adding fontsize to JavaScript paragraph
+document.getElementById('JavaScript').style.fontSize='35px';
+
